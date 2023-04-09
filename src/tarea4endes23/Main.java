a94e3490bde5bb385aa91c6463a5470ac56df1a2
JANV <81357115+jhax311@users.noreply.github.com>
2023-04-09 19:00:57 +0200

    Version1.0

---
+++ src/tarea4endes23/Main.java
@@ -0,0 +1,34 @@
+
+package tarea4endes23;
+/**
+ *
+ * @author Jhoel Alexander Narváez Valarezo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       CCuenta miCuenta;
+        double saldoActual;
+        
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+    
+    }
+    
+    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
+        c.ingresar(cantidad);
+        
+    }
+    
+    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
+        c.retirar(cantidad);
+    }  
+        
+        
+        
+    }
+
+
